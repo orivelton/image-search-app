a9bf9d66c17175cccd6610786c9ab89621011780
Orivelton Cesar Da Conceição <oriveltoncesar@MacBook-Air-de-Orivelton.local>
2018-05-26 14:01:12 -0300

    Adjustemed image service

--- src/app/image-list/image-list.component.ts
+++ src/app/image-list/image-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ImageService } from './shared/image.service';
+import { ImageService } from '../shared/image.service';
 @Component({
   selector: 'app-image-list',
   templateUrl: './image-list.component.html',
@@ -8,6 +8,14 @@ import { ImageService } from './shared/image.service';
 export class ImageListComponent implements OnInit {
   images: any[];
   constructor(private _imageService: ImageService) { }
+
+  searchImage(query: string) {
+     return this._imageService.getImages(query).subscribe(
+       data => console.log(data),
+       error => console.log(error),
+       () => console.log('Request complete')
+     );
+  }
 
   ngOnInit() {
   }
--- src/app/shared/image.service.ts
+++ src/app/shared/image.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { environment } from './../../environments/environment';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 
 
 @Injectable()
@@ -10,12 +11,12 @@ export class ImageService {
   private API_KEY: string = environment.PIXABAY_API_KEY;
   private API_URL: string = environment.PIXABAY_API_URL;
   private URL: string = this.API_URL + this.API_KEY + '&q=';
-  private perPage: string = '&per_page=10';
+  private perPage: any = '&per_page=10';
 
   constructor (private _http: Http) {}
 
   getImages(query) {
     return this._http.get(this.URL + query + this.perPage)
-    .map(return => res.json());
+    .map(res => res.json());
   }
 }
