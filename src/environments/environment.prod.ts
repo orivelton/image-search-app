0348fef42456f2bcabf7b2e76b96854ccf4bf769
Orivelton Cesar Da Conceição <oriveltoncesar@MacBook-Air-de-Orivelton.local>
2018-05-27 14:55:02 -0300

    add api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  roduction: false,
+  PIXABAY_API_KEY: '9103424-00ba5d3d7846f2b332342500d',
+  PIXABAY_API_URL: 'https://pixabay.com/api/?key='
 };
